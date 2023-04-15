84e52e26aa97ee99fa058627fcbc46467da45406
andrebritoassumpcao <andrebritoassumpcao@outlook.com>
2023-04-15 20:21:12 -0300

    Add the total pontuation

--- Program.cs
+++ Program.cs
@@ -4,26 +4,41 @@ namespace JogoDeAdivinhacao {
     class Program {
         static void Main(string[] args) {
             Random random = new Random();
-            int numeroAleatorio = random.Next(1, 101);
-            int numeroTentativas = 0;
-            int palpite = 0;
+            int pontuacaoTotal = 0;
+            bool tentarNovamente = true;
 
-            Console.WriteLine("Bem-vindo! Esse é o jogo de advinhação, boa sorte!!");
-            Console.WriteLine("Digite aqui um número entre 0 e 100:");
+            while (tentarNovamente) { 
+                int numeroAleatorio = random.Next(1, 101);
+                int numeroTentativas = 0;
+                int palpite = 0;
 
-            while (palpite != numeroAleatorio) {
-                numeroTentativas++;
+                Console.WriteLine("Bem-vindo! Esse é o jogo de advinhação, boa sorte!!");
+                Console.WriteLine("Digite aqui um número entre 0 e 100:");
 
-                palpite = int.Parse(Console.ReadLine());
+                while (palpite != numeroAleatorio) {
+                    numeroTentativas++;
 
-                if (palpite == numeroAleatorio) {
-                    Console.WriteLine("Uaaauu, meus parabéns você acertou!!");
+                    palpite = int.Parse(Console.ReadLine());
+
+                    if (palpite == numeroAleatorio) {
+                        Console.WriteLine("Uaaauu, meus parabéns você acertou!!");
+                        Console.WriteLine("Suas tentativas: " + numeroTentativas);
+
+                        pontuacaoTotal += (10 - numeroTentativas);
+                        Console.WriteLine($"Sua pontuação nesta rodada é: {10 - numeroTentativas}");
+
+                        Console.WriteLine("Deseja jogar novamente? (s/n)");
+                        tentarNovamente = Console.ReadLine().ToLower() == "s";
+
+                    } else if (palpite > numeroAleatorio) {
+                        Console.WriteLine("Hmm, quase!! O número certo é menor!");
+                    } else {
+                        Console.WriteLine("Hmm, quase!! O número certo é maior!");
+                    }
                 }
-                else if (palpite > numeroAleatorio){
-                    Console.WriteLine("Hmm, quase!! O número certo é menor!");
-                } else {
-                    Console.WriteLine("Hmm, quase!! O número certo é maior!");
 
+                if (!tentarNovamente) {
+                    Console.WriteLine($"Sua pontuação total é: {pontuacaoTotal}");
                 }
             }
 
